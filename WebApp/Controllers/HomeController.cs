607798ad4c2874fea7a4f1a385b09dd471f432b7
FagnerLinhares <fagnerlinhares7@gmail.com>
2025-03-31 17:10:06 -0400

    Adicionei formulario de nome e idade

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //Action
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(string nome, string sobrenome, int idade) 
+        {
+            //
+            return View("Privacy");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
